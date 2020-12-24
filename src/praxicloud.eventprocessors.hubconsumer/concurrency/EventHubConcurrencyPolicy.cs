ae8412d8dd584417eaa2df233a63f42043fa32d4
chrisgclayton <cclayton@microsoft.com>
2020-12-24 11:38:32 -0500

    Added ease of use classes for Event Hub and IoT Hub concurrency

---
+++ src/praxicloud.eventprocessors.hubconsumer/concurrency/EventHubConcurrencyPolicy.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Christopher Clayton. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace praxicloud.eventprocessors.hubconsumer.concurrency
+{
+    #region Using Clauses
+    using praxicloud.eventprocessors.hubconsumer.partitioners;
+    #endregion
+
+    /// <summary>
+    /// A concurrency policy specifically targetting Event Hub and the use of the partition key as the partitioner. This will not work properly if the producer does not set the partition key.
+    /// </summary>
+    public sealed class EventHubConcurrencyPolicy : PartitionedMaximumConcurrencyPolicy
+    {
+        /// <summary>
+        /// Initializes a new instance of the type
+        /// </summary>
+        /// <param name="maximumDegreeOfParallelism">The maximum degree of parallelism allowed</param>
+        public EventHubConcurrencyPolicy(short maximumDegreeOfParallelism) : base(maximumDegreeOfParallelism, new DefaultEventHubPartitioner())
+        {
+        }
+    }
+}
---
+++ src/praxicloud.eventprocessors.hubconsumer/concurrency/IoTHubConcurrencyPolicy.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Christopher Clayton. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace praxicloud.eventprocessors.hubconsumer.concurrency
+{
+    #region Using Clauses
+    using praxicloud.eventprocessors.hubconsumer.partitioners;
+    #endregion
+
+    /// <summary>
+    /// A concurrency policy specifically targetting IoT Hub and the use of the device id as the partitioner. 
+    /// </summary>
+    public sealed class IoTHubConcurrencyPolicy : PartitionedMaximumConcurrencyPolicy
+    {
+        /// <summary>
+        /// Initializes a new instance of the type
+        /// </summary>
+        /// <param name="maximumDegreeOfParallelism">The maximum degree of parallelism allowed</param>
+        public IoTHubConcurrencyPolicy(short maximumDegreeOfParallelism) : base(maximumDegreeOfParallelism, new DefaultIoTHubPartitioner())
+        {
+        }
+    }
+}
